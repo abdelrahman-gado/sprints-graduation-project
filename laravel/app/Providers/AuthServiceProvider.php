2413c53b8884a02fb54bd8143bd461862a5ec093
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2023-03-02 10:50:02 +0200

    define authorization gate in AuthServiceProvider for admin user

--- laravel/app/Providers/AuthServiceProvider.php
+++ laravel/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,9 @@ class AuthServiceProvider extends ServiceProvider
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
-        //
+        // authorization for admin
+        Gate::define('is_admin', function (User $user) {
+            return $user->is_admin === 1;
+        });
     }
 }
