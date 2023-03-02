9c66a960e1e829ae186c5e5b5be7f232777a1b7e
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2023-03-02 10:47:18 +0200

    create Order Details Model

---
+++ laravel/app/Models/Order_Detail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order_Detail extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'order_details';
+
+    protected $fillable = [
+        'product_id',
+        'order_id',
+        'price',
+        'quantity',
+        'subTotalPrice'
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class, 'order_id', 'id');
+    }
+
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+}
